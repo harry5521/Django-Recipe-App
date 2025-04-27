119bc9edc5bb27f7518f0642aff40f094c56d50d
Abu Huraira <127491076+harry5521@users.noreply.github.com>
2025-04-27 15:15:21 +0500

    Delete student_card directory

--- student_card/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from home import views
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('marks/<std_id>/', views.marks, name='view_marks'),
-]
